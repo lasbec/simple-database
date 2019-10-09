89c7f1acd7b9e77401faf04050b2abd68e111a5e
Enert-PC\Enert <lasse@cpsh.de>
2019-10-09 17:12:31 +0200

    first database tests exceeded, now starting with persistent objects

--- sdb/database.py
+++ sdb/database.py
@@ -1,11 +1,131 @@
 from collections.abc import MutableMapping
+from ZODB import FileStorage, DB
+import transaction
+import os
+
 
 
 class DataBase(MutableMapping):
-    pass
+    """An Object representing a database."""
+
+    def __init__(self, full_path, auto_open=True):
+        """"""  # todo writing proper docstrings
+        self._full_path = full_path + '.fs'
+        self._storage = None
+        self._db = None
+        self._connection = None
+        self._root = None
+        if auto_open:
+            self.open()
+
+    @property
+    def full_path(self):
+        return self._full_path[:-3]
+
+    @property
+    def name(self):
+        return self.full_path.split('\\')[-1]
+
+    @property
+    def path(self):
+        return '\\'.join(self.full_path.split('\\')[:-1])
+
+    @property
+    def is_opened(self):
+        """"""  # todo docstings schreiben
+        assert ((self._db is not None
+                 and self._root is not None
+                 and self._connection is not None
+                 and self._storage is not None)
+                !=
+                (self._db is None
+                 and self._root is None
+                 and self._connection is None
+                 and self._storage is None)
+                )
+        return self._db is not None
+
+    @property
+    def is_closed(self):
+        """"""  # todo docstings schreiben
+        return not self.is_opened
+
+    def __enter__(self):
+        self.open()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def __delitem__(self, key):
+        """"""  # todo docstrings
+        del self._root[key]
+
+    def __getitem__(self, item):
+        """"""  # todo docstrings
+        return self._root[item]
+
+    def __iter__(self):
+        """"""  # todo docstrings
+        return self._root.__iter__()
+
+    def __len__(self):
+        """"""  # todo docstrings
+        return len(self._root)
+
+    def __setitem__(self, key, value):
+        """"""  # todo docstrings
+        self._root[key] = value
+
+    def _total_extinction(self):
+        """"""  # todo docstrings
+        self.close()
+        os.remove(self._full_path)
+        os.remove(self._full_path + '.index')
+        os.remove(self._full_path + '.lock')
+        os.remove(self._full_path + '.tmp')
+
+    def keys(self):
+        """"""  # todo docstrings
+        return self._root.keys()
+
+    def values(self):
+        """"""  # todo docstrings
+        return self._root.values()
+
+    def open(self):
+        """"""  # todo docstrings
+        if self.is_opened:
+            return 1
+        else:
+            self._storage = FileStorage.FileStorage(self._full_path)
+            self._db = DB(self._storage)
+            self._connection = self._db.open()
+            self._root = self._connection.root()
+            return 0
+
+    def close(self):
+        """"""  # todo docstrings
+        if self.is_closed:
+            return 1
+        else:
+            transaction.commit()
+            self._connection.close()
+            self._db.close()
+            self._storage.close()
+            self._storage = None
+            self._db = None
+            self._connection = None
+            self._root = None
+            return 0
+
+
+
 
 
 class NotExistingDataBaseException(Exception):
+    """"""  # todo docstrings
     # todo wie mache ich eine richtige Exception
     def __init__(self):
+        """"""  # todo docstrings
         pass
--- sdb/database_test.py
+++ sdb/database_test.py
@@ -1,80 +1,143 @@
 import unittest
+from unittest import TestCase
 import os
-from .database import DataBase, NotExistingDataBaseException
+from database import DataBase, NotExistingDataBaseException
 
 
-class DataBaseTest0(unittest.TestCase):
+class DataBaseTest0(TestCase):
     db_path = 'tests'
-    db_name = 'test_base'
+    db_name = 'general_test_base'
+
+    def __init__(self, *args, **kwargs):
+        TestCase.__init__(self, *args, **kwargs)
+        self.immutables = [True, False,
+                           None,
+                           7357, 42,
+                           (True, 69), (1337, ()),
+                           '\n +  \t', 'test',
+                           frozenset([1, "a", "\n"]), frozenset([1, ('r', 2), "a", "\n"])]
+        self.db_path = 'tests'
+        self.db_name = 'general_test_base'
+        self.full_db_path = self.db_path + '\\' + self.db_name
+
+    def setUp(self):
+        self.db = DataBase(self.full_db_path)
+
+    def tearDown(self):
+        self.db._total_extinction()
 
     def assert_db_is_proper_closed(self, db):
-        self.assertIsNone(db.db)
-        self.assertIsNone(db.root)
-        self.assertIsNone(db.storage)
-        self.assertIsNone(db.connection)
+        self.assertIsNone(db._db)
+        self.assertIsNone(db._root)
+        self.assertIsNone(db._storage)
+        self.assertIsNone(db._connection)
         self.assertTrue(db.is_closed)
-        self.assertFalse(db.is_opend)
+        self.assertFalse(db.is_opened)
 
     def assert_db_is_proper_opened(self, db):
-        self.assertIsNotNone(db.db)
-        self.assertIsNotNone(db.root)
-        self.assertIsNotNone(db.storage)
-        self.assertIsNotNone(db.connection)
+        self.assertIsNotNone(db._db)
+        self.assertIsNotNone(db._root)
+        self.assertIsNotNone(db._storage)
+        self.assertIsNotNone(db._connection)
         self.assertFalse(db.is_closed)
-        self.assertTrue(db.is_opend)
+        self.assertTrue(db.is_opened)
 
     def test_basics(self):
+        """Tests creating, opening, closing and deleting a database."""
         # test the creation for a database
-        full_path = DataBaseTest0.db_path + '\\' + DataBaseTest0.db_name
+        db_path = 'tests'
+        db_name = 'basic_tests'
+        full_path = db_path + '\\' + db_name
         db = DataBase(full_path)
 
-        self.assertEquals(db.name, DataBaseTest0.db_name)
-        self.assertEquals(db.path, DataBaseTest0.db_path)
-        self.assertEquals(db.full_path, full_path)
-        self.assertTrue(os.path.exists(DataBaseTest0.db_path + '.fs'))
-        self.assertTrue(os.path.exists(DataBaseTest0.db_path + '.fs.index'))
-        self.assertTrue(os.path.exists(DataBaseTest0.db_path + '.fs.lock'))
-        self.assertTrue(os.path.exists(DataBaseTest0.db_path + '.fs.tmp'))
+        self.assertEqual(db.name, db_name)
+        self.assertEqual(db.path, db_path)
+        self.assertEqual(db.full_path, full_path)
+        self.assertTrue(os.path.exists(full_path + '.fs'))
+        self.assertTrue(os.path.exists(full_path + '.fs.index'))
+        self.assertTrue(os.path.exists(full_path + '.fs.lock'))
+        self.assertTrue(os.path.exists(full_path + '.fs.tmp'))
 
         # test closing the DataBase object
         ret = db.close()
-        self.assertEquals(ret, 0)
+        self.assertEqual(ret, 0)
         self.assert_db_is_proper_closed(db)
 
         # test recloseing the DataBase object
         ret = db.close()
-        self.assertAlmostEquals(ret, 1)
+        self.assertEqual(ret, 1)
         self.assert_db_is_proper_closed(db)
 
         # test opening a DataBase object
-        ret = db.opend()
-        self.assertEquals(ret, 0)
+        ret = db.open()
+        self.assertEqual(ret, 0)
         self.assert_db_is_proper_opened(db)
 
         # test reopeing a DataBase object
-        ret = db.opend()
-        self.assertEquals(ret, 1)
+        ret = db.open()
+        self.assertEqual(ret, 1)
         self.assert_db_is_proper_opened(db)
 
         # test _total_extinction of the database
         db._total_extinction()
 
-        self.assertFalse(os.path.exists(DataBaseTest0.db_path + '.fs'))
-        self.assertFalse(os.path.exists(DataBaseTest0.db_path + '.fs.index'))
-        self.assertFalse(os.path.exists(DataBaseTest0.db_path + '.fs.lock'))
-        self.assertFalse(os.path.exists(DataBaseTest0.db_path + '.fs.tmp'))
+        self.assertFalse(os.path.exists(full_path + '.fs'))
+        self.assertFalse(os.path.exists(full_path + '.fs.index'))
+        self.assertFalse(os.path.exists(full_path + '.fs.lock'))
+        self.assertFalse(os.path.exists(full_path + '.fs.tmp'))
 
-        # test acessing an attribute from an extinctioned database
-        for attr in dir(db):
-            self.assertRaises(NotExistingDataBaseException, getattr, db, attr)
-            warning = ' '  # todo waring regex
-            self.assertWarnsRegex(warning, getattr, db, attr)
+        # test recreating database
+        ret = db.open()
+        self.assertEqual(ret, 0)
+        self.assert_db_is_proper_opened(db)
 
-        # test deleting the object
-        test_var = db
-        del db
-        try:
-            test_var
-            self.assertTrue(False)
-        except NameError:
-            self.assertTrue(True)
+        db._total_extinction()
+
+    def test_context_manager(self):
+        """"""  # todo docstrings schreiben
+        db_path = 'tests'
+        db_name = 'context_manager_test'
+        full_path = db_path + '\\' + db_name
+        immutables_rev = list(reversed(self.immutables))
+        test_root = zip(immutables_rev, self.immutables)
+
+        # writing data into database
+        with DataBase(full_path) as db:
+            self.assert_db_is_proper_opened(db)
+            for key, value in test_root:
+                db[key] = value
+
+        # ensure that the database was closed by checking if accessing is possible
+        db = DataBase(full_path)
+
+        # checking correctness and completeness of stored data
+        for key, value in test_root:
+            self.assertIn(key, db.keys())
+            self.assertIn(value, db.values())
+            self.assertEqual(db[key], value)
+
+        # manual teardown
+        db._total_extinction()
+
+    def test_auto_open(self):
+        """Testing the constructor on the functionality of the parameter auto_open."""
+        # todo implement
+
+    def test_simple_immutable_storage(self):
+        """Tests persistence of little amount of immutable objects when stored in a DataBase object."""
+        immutables_rev = list(reversed(self.immutables))
+        test_root = zip(immutables_rev, self.immutables)
+        for key, value in test_root:
+            self.db[key] = value
+
+        self.db.close()
+        self.db.open()
+
+        for key, value in test_root:
+            self.assertIn(key, self.db.keys())
+            self.assertIn(value, self.db.values())
+            self.assertEqual(self.db[key], value)
+
+
+if __name__ == '__main__':
+    unittest.main()
